0108a0b8f38e407bce0e6fceee9268c30c342002
Duy Nguyen <nguyen48@stolaf.edu>
2022-11-20 14:54:14 -0600

    sound array check added

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const draggableElements = document.querySelectorAll(".draggable");
 const droppableElements = document.querySelectorAll(".droppable");
 var x = document.getElementById("CapstoneAudio"); 
-var y = document.getElementById("CapstoneAudio2"); 
+var y = document.getElementById("CapstoneAudio2");
+var z = [] 
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
@@ -30,6 +31,36 @@ function playAudio2() {
   y.play(); 
 } 
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function playAllAudio() {
+  const filteredArray = z.filter(element => element!=0);
+  console.log(filteredArray);
+  for (element of filteredArray) {
+    if (element === 1) {
+      x.play();
+      await sleep(x.duration);
+    }
+    else if (element === 2) {
+      y.play();
+      await sleep(y.duration);
+    }
+  }
+  // filteredArray.forEach(
+  //   async element => {
+  //     if (element === 1) {
+  //       x.play();
+  //       await sleep(x.duration);
+  //     }
+  //     else if (element === 2) {
+  //       y.play();
+  //       await sleep(y.duration);
+  //     }
+  //   }
+  // )
+}
 function dragStart(event) {
   event.dataTransfer.setData("text", event.target.id); }
 
@@ -63,13 +94,24 @@ function drop(event) {
   event.target.classList.remove("droppable-hover");
   const draggableElementData = event.dataTransfer.getData("text"); 
   const droppableElementData = event.target.getAttribute("data-draggable-id");
+  const droppableElementId = event.target.getAttribute("id");
+  const matchingDroppableElement = document.getElementById(((Number(droppableElementId)+10)%20).toString());
+  // console.log(((Number(event.target.getAttribute("id"))+10)%20).toString()).toString());
   const isCorrectMatching = draggableElementData === droppableElementData;
   if(isCorrectMatching) {
+    // console.log("running cond");
     const draggableElement = document.getElementById(draggableElementData);
     event.target.classList.add("dropped");
     event.target.style.backgroundColor = window.getComputedStyle(draggableElement).color;
     // draggableElement.classList.add("dragged");
     // draggableElement.setAttribute("draggable", "false");
     event.target.insertAdjacentHTML("afterbegin", `<i class ="fas fa-${draggableElementData} " draggable="true"></i>`);
+    z[Number(droppableElementId)%10-1]=Math.floor(Number(droppableElementId)/10+1);
+    // console.log(z);
+    if(matchingDroppableElement.classList.contains("dropped")) {
+      matchingDroppableElement.classList.remove("dropped");
+      matchingDroppableElement.style.backgroundColor = "white";
+      matchingDroppableElement.removeChild(matchingDroppableElement.firstChild);
+    };
   }
 }
